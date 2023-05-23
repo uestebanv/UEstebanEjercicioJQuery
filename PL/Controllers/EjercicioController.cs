aaf38d57749297e8d23a498ba1ca7d4bdadc89a1
uestebanv <ulisesestebanvaldes98@gmail.com>
2023-05-22 18:10:42 -0600

    Add project files.

---
+++ PL/Controllers/EjercicioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class EjercicioController : Controller
+    {
+        // GET: Ejercicio
+        public ActionResult Tabla()
+        {
+            return View();
+        }
+    }
+}
